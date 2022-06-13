c6e8cfadc2c105fb9bdca5d098d86948515bd37d
mlcas <martincastro88@hotmail.com>
2022-06-13 08:08:27 -0700

    uploading home pagination

--- frontend/src/components/DealCards/index.js
+++ frontend/src/components/DealCards/index.js
@@ -5,11 +5,23 @@ import "./DealCards.css";
 
 const DealsCard = () => {
   const [gameDeals, setGameDeals] = useState([]);
+  const [page, setPage] = useState(0);
 
+  const nextPageHandler = () => {
+    setPage((prevState) => prevState + 1);
+    console.log(page);
+  };
+
+  const prevPageHandler = () => {
+    if (page >= 1) {
+      setPage((prevState) => prevState - 1);
+    }
+
+    console.log(page);
+  };
   useEffect(() => {
     const consultarApi = async () => {
-      const url =
-        "https://www.cheapshark.com/api/1.0/deals?storeID=1&pageSize=5&pageNumber=0&onSale=1";
+      const url = `https://www.cheapshark.com/api/1.0/deals?storeID=1&pageSize=5&pageNumber=${page}&onSale=1`;
 
       const { data } = await axios(url);
 
@@ -17,12 +29,22 @@ const DealsCard = () => {
       console.log(gameDeals);
     };
     consultarApi();
-  }, []);
+  }, [page]);
 
   return (
     <>
       <div className="container-deals">
-        <p className="text-DealsCard">Deals</p>
+        <div className="container-arrowHeader">
+          <p className="text-DealsCard">Deals</p>
+          <div className="container-arrow">
+            <button className="arrow" onClick={prevPageHandler}>
+              {"<"}
+            </button>
+            <button className="arrow" onClick={nextPageHandler}>
+              {">"}
+            </button>
+          </div>
+        </div>
         <div className="container-dealsCard">
           {gameDeals.map((item) => (
             <Cards
