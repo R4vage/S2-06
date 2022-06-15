c64ca91b60b921cfeb8421901bbd86aeda305d8d
mlcas <martincastro88@hotmail.com>
2022-06-15 12:52:04 -0700

    library

--- frontend/src/components/Card/index.js
+++ frontend/src/components/Card/index.js
@@ -1,50 +1,56 @@
 import React from "react";
 import "./Card.css";
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import install from "../../assets/icons/install.svg";
 
 const Card = (props) => {
-const navigate = useNavigate()
-const userGames = useSelector((state) => state.gamesArray);
-var isOwned = false
-if(userGames?.length !==0) { isOwned = !!userGames?.find(item => item.gameID === Number(props.steamAppID));}
-
-
-  function onClick (){
-    navigate(`/games/${props.steamAppID}`)
+  const navigate = useNavigate();
+  const userGames = useSelector((state) => state.gamesArray);
+  var isOwned = false;
+  if (userGames?.length !== 0) {
+    isOwned = !!userGames?.find((item) => item.gameID === Number(props.steamAppID));
   }
-  return (<>
-    {props.steamAppID && <div className="conteiner-Card" onClick={()=>onClick()}>
-      <div>
-        <img
-          className="img"
-          src={`https://steamcdn-a.akamaihd.net/steam/apps/${props.steamAppID}/header.jpg`}
-          alt="image"
-        />
-      </div>
-      <p className="text-card">{props.name}</p>
-      
-      {props.inLibrary? <button className="isOwnedButton">Instalar</button>: 
-      isOwned? <button className="isOwnedButton">Owned</button>:
-      (Math.round(props.savings) ? (
-        <div className="details">
-          <p className="discount">{props.salePrice}</p>
-          <p className="price">{props.normalPrice}</p>
-          <p className="percentage">-{Math.round(props.savings)}%</p>
+
+  function onClick() {
+    navigate(`/games/${props.steamAppID}`);
+  }
+  return (
+    <>
+      {props.steamAppID && (
+        <div className="conteiner-Card" onClick={() => onClick()}>
+          <div>
+            <img
+              className="card-img"
+              src={`https://steamcdn-a.akamaihd.net/steam/apps/${props.steamAppID}/header.jpg`}
+              alt="image"
+            />
+          </div>
+          <p className="text-card">{props.name}</p>
+
+          {props.inLibrary ? (
+            <div className="container-install">
+              <img className="img-install" src={install} alt="inst" />
+              <p className="install">Install</p>
+            </div>
+          ) : isOwned ? (
+            <button className="isOwnedButton">Owned</button>
+          ) : Math.round(props.savings) ? (
+            <div className="details">
+              <p className="discount">{props.salePrice}</p>
+              <p className="price">{props.normalPrice}</p>
+              <p className="percentage">-{Math.round(props.savings)}%</p>
+            </div>
+          ) : (
+            <p className="discount">{props.normalPrice}</p>
+          )}
         </div>
-      ) : (
-        <p className="discount">{props.normalPrice}</p>
-      ))
-      
-      }
-    </div>}
+      )}
     </>
   );
 };
 
 export default Card;
-
-
 
 /* function UrlExists() {
   var isImg = false
--- frontend/src/components/ListLibraryCards/ListLibraryCards.js
+++ frontend/src/components/ListLibraryCards/ListLibraryCards.js
@@ -1,23 +1,29 @@
-import "./ListLibraryCards.css"
+import "./ListLibraryCards.css";
 import Cards from "../Card";
+import icon from "../../assets/icons/tagsUnder.svg";
 
 function ListLibraryCards(props) {
-   const {games} = props
-    return (  
-        <div className="library">
-            {games?.map((item) => (
-                <Cards
-                steam={item.gameID}
-                name={item.gameName}
-                key={item.gameID}
-                steamAppID={item.gameID}
-                isOwned={true}
-                inLibrary={true}
-                />
-            ))}
-        </div>
-
-    );
+  const { games } = props;
+  return (
+    <>
+      <div className="container-sort">
+        <h3 className="sort">Sort By: Alphabetical</h3>
+        <img className="" src={icon} alt="icon" />
+      </div>
+      <div className="library">
+        {games?.map((item) => (
+          <Cards
+            steam={item.gameID}
+            name={item.gameName}
+            key={item.gameID}
+            steamAppID={item.gameID}
+            isOwned={true}
+            inLibrary={true}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default ListLibraryCards;
