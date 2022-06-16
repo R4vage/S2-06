3fb1ef0bb6c5e32235c5da2c6def15b2ca095e01
mlcas <martincastro88@hotmail.com>
2022-06-16 08:52:30 -0700

    sidebar

--- frontend/src/routes/PrivateRouter.js
+++ frontend/src/routes/PrivateRouter.js
@@ -1,28 +1,18 @@
-
 import { Route, Routes } from "react-router-dom";
-import Navbar from "../components/Navbar";
+//import Navbar from "../components/Navbar";
 import PrivateHome from "../pages/Private/PrivateHome";
 import PrivateProfile from "../pages/Private/PrivateProfile";
 import Footer from "../components/Footer";
 
-
-
-
-
 function PrivateRouter() {
-    return ( 
-
-        <>
-            <Navbar />
-            <Routes>
-                <Route exact match path="/" element={<PrivateHome />}></Route>
-                <Route exact match path="profile" element={<PrivateProfile />}></Route>
-            </Routes>
-            <Footer />
-        </>
-     );
-
-
+  return (
+    <>
+      <Routes>
+        <Route exact match path="/" element={<PrivateHome />} />
+        <Route exact match path="profile" element={<PrivateProfile />} />
+      </Routes>
+    </>
+  );
 }
 
 export default PrivateRouter;
